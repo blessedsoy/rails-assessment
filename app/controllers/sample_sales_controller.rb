5cf71589c5f1da7cd7c492d81497972df801fbd0
blessedsoy <unmigong@gmail.com>
2016-09-01 15:33:19 -0400

    moved add_to_list method from sample_sale controller to lists_ controller create method

--- app/controllers/sample_sales_controller.rb
+++ app/controllers/sample_sales_controller.rb
@@ -1,58 +1,76 @@
 class SampleSalesController < ApplicationController
   def index
     @sample_sales = SampleSale.all 
+    @list = List.new
   end
 
   def show
-    @sample_sale = SampleSale.find(params[:id])
-  end
-
-  def add_to_list
-    if !current_user
-      redirect_to new_user_session_path
+    if current_user
+      @sample_sale = SampleSale.find(params[:id])
     else
-      if params[:sample_sales_ids] || current_user.lists
-        if params[:sample_sales_ids]
-          @list = List.create_from_sample_sales(current_user.id, params[:sample_sales_ids]) 
-        else
-          @list = current_user.lists 
-        end
-        redirect_to lists_path
-      else
-        redirect_to sample_sales_path 
-      end
+      flash[:error] = "You need to sign in or sign up before continuing."
+      redirect_to new_user_session_path
     end
   end
 
+  # def add_to_list
+  #   if !current_user
+  #     redirect_to new_user_session_path
+  #   else
+  #     if params[:sample_sales_ids] || current_user.lists
+  #       if params[:sample_sales_ids]
+  #         @list = List.create_from_sample_sales(current_user.id, params[:sample_sales_ids]) 
+  #       else
+  #         @list = current_user.lists 
+  #       end
+  #       redirect_to lists_path
+  #     else
+  #       redirect_to sample_sales_path 
+  #     end
+  #   end
+  # end
+
   def sort_by_likes
     @sample_sales = SampleSale.sort_by_likes
+    @list = List.new
     render '/sample_sales/index'
   end
 
   def like
-    get_vote
-    
-    if @vote.like == 0 && @vote.dislike != -1
-      @vote.like += 1
-    elsif @vote.like == 1 
-      @vote.like -= 1 
-    end
-    @vote.save
+    if current_user
+      get_vote
 
-    redirect_to sample_sales_path
+      if @vote.like == 0 && @vote.dislike != -1
+        @vote.like += 1
+      elsif @vote.like == 1 
+        @vote.like -= 1 
+      end
+      @vote.save
+
+      redirect_to sample_sales_path
+    else 
+      flash[:error] = "You need to sign in or sign up before continuing."
+      redirect_to new_user_session_path
+    end
   end
 
   def dislike
-    get_vote
-    
-    if @vote.dislike == 0 && @vote.like != 1
-      @vote.dislike -= 1 
-    elsif @vote.dislike == -1 
-      @vote.dislike += 1  
-    end
-    @vote.save
+    if current_user
+      get_vote
+      
+      if @vote.dislike == 0 && @vote.like != 1
+        @vote.dislike -= 1 
+      elsif @vote.dislike == -1 
+        @vote.dislike += 1  
+      end
+      @vote.save
 
-    redirect_to sample_sales_path
+      redirect_to sample_sales_path
+    else
+      flash[:error] = "You need to sign in or sign up before continuing."
+      redirect_to new_user_session_path
+    end
+
   end
 
 private
